9d092271210ce7ed195f158d072d6cda471497fc
Renee <reneerocha.pi@gmail.com>
2021-05-16 22:58:35 -0300

    add describe

--- spec/lib/hero_spec.rb
+++ spec/lib/hero_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 require 'hero'
 
 describe Hero do
+    describe "quando está com a armadura" do
+        
     it "has a sword" do
         hero = Hero.new
         expect(hero.weapon).to eq('sword')    
@@ -9,5 +11,6 @@ describe Hero do
     it "has more than hp 1000 points" do
         hero = Hero.new
         expect(hero.hp).to be > (1000)
+    end
     end
 end
